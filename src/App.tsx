39dd21dc47dacaa1029784c1609b079cb76f4374
Sifen Abduselam <sifenabduselam7@gmail.com>
2025-08-07 08:42:41 -0700

    working on project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ResumeFix from './components/ResumeFix';
+
+function App() {
+  return (
+    <div className="App">
+      <ResumeFix />
+    </div>
+  );
+}
+
+export default App;
